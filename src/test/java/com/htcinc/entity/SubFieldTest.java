f13e887c608bd21c1b33fac78ff51bc2692d05d2
kirankumar-akula <kumar.kiran4003@gmail.com>
2016-10-26 13:04:12 +0530

    created marshalling test cases

--- src/test/java/com/htcinc/entity/SubFieldTest.java
+++ src/test/java/com/htcinc/entity/SubFieldTest.java
@@ -19,4 +19,13 @@ public class SubFieldTest {
         String subFieldCode = subField.getCode();
         assertNotNull(subFieldCode);
     }
+    @Test
+    public void checkDescription() throws Exception {
+        SubField subField = new SubField();
+        subField.setDescription("desc");
+        String description = subField.getDescription();
+        assertNotNull(description);
+
+    }
+
 }
